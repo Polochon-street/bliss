5fd086f38030e17ed9141261d31325bacc12ea2b
Phyks (Lucas Verney) <phyks@phyks.me>
2015-11-23 23:08:15 +0100

    Do not execute testing code if not directly ran

--- python/bliss.py
+++ python/bliss.py
@@ -1,6 +1,5 @@
 from _bliss import ffi, lib
 import collections
-import json
 
 
 class bl_song(collections.abc.Mapping):
@@ -243,30 +242,34 @@ def cosine_similarity(filename1, filename2):
     }
 
 
-song = bl_song()
-song.set("artist", "foobar")
-song.set("force", 1)
-print(bl_song)
-print(song.get("artist"))
-print(str(song.get("force")))
-print(song["artist"])
-song["artist"] = "foo"
-print(song["artist"])
+if __name__ == "__main__":
+    import json
 
-song = bl_song("/tmp/test.mp3")
-print(song["genre"])
-song.free()
-
-for key in song:
-    print(key)
-    print("ok")
-
-print(distance("/tmp/test.mp3", "/tmp/test.mp3")["distance"])
-print(cosine_similarity("/tmp/test.mp3", "/tmp/test.mp3")["similarity"])
-
-with bl_song("/tmp/test.mp3") as song:
+    # Some testing and debug code
+    song = bl_song()
+    song.set("artist", "foobar")
+    song.set("force", 1)
+    print(bl_song)
+    print(song.get("artist"))
+    print(str(song.get("force")))
     print(song["artist"])
-    print(song["force_vector"])
-    #song["sample_array"] = []
-    #print(dict(song))
-    #print(json.dumps(song))
+    song["artist"] = "foo"
+    print(song["artist"])
+
+    song = bl_song("/tmp/test.mp3")
+    print(song["genre"])
+    song.free()
+
+    for key in song:
+        print(key)
+        print("ok")
+
+    print(distance("/tmp/test.mp3", "/tmp/test.mp3")["distance"])
+    print(cosine_similarity("/tmp/test.mp3", "/tmp/test.mp3")["similarity"])
+
+    with bl_song("/tmp/test.mp3") as song:
+        print(song["artist"])
+        print(song["force_vector"])
+        # song["sample_array"] = []
+        # print(dict(song))
+        # print(json.dumps(song))
