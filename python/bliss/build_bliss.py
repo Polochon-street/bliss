a143c8e53d2f6457b46bbd5f97dbccb6c9d36609
Phyks (Lucas Verney) <phyks@phyks.me>
2015-12-06 15:53:58 +0100

    Fix build with swresample/avresample

--- python/bliss/build_bliss.py
+++ python/bliss/build_bliss.py
@@ -1,16 +1,28 @@
+import sys
+
 from cffi import FFI
+from buildtools import pkgconfig
 
 ffi = FFI()
 
+# Check which resample lib to use
+if pkgconfig.exists("libswresample"):
+    resample = ("../src/decode.c", "swresample")
+elif pkgconfig.exists("libavresample"):
+    resample = ("../src/decode_av.c", "avresample")
+else:
+    sys.exit("No libswresample/libavresample available.")
+
+# Build
 ffi.set_source("bliss._bliss",
                "#include \"bliss.h\"",
                sources=["../src/amplitude_sort.c",
                         "../src/analyze.c",
-                        "../src/decode.c",
+                        resample[0],
                         "../src/envelope_sort.c",
                         "../src/frequency_sort.c",
                         "../src/helpers.c"],
-               libraries=["avformat", "avutil", "avcodec"],
+               libraries=["avformat", "avutil", "avcodec", resample[1]],
                include_dirs=["/usr/include/ffmpeg/", "../include/"])
 
 header = '\n'.join([i for i in open("../include/bliss.h", 'r').readlines()
