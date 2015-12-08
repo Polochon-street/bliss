1448d364f23c2255648bd48b5ce2269bb73b8920
Phyks (Lucas Verney) <phyks@phyks.me>
2015-12-08 15:13:30 +0100

    Export constants to Python bindings

--- python/bliss/__init__.py
+++ python/bliss/__init__.py
@@ -5,5 +5,9 @@ import bliss.version as version
 from bliss._bliss import lib
 
 BL_LOUD = lib.BL_LOUD
+BL_CALM = lib.BL_CALM
+BL_UNKNOWN = lib.BL_UNKNOWN
+BL_UNEXPECTED = lib.BL_UNEXPECTED
+BL_OK = lib.BL_OK
 
 __all__ = ["bl_song", "distance", "version", "BL_LOUD"]
