72735c2ca92bc6dde6810cd583e2ab31095be1ff
Polochon-street <polochonstreet@gmx.fr>
2015-12-16 00:42:31 +0100

    Fixed some memory leaks (wip)

--- examples/distance.c
+++ examples/distance.c
@@ -16,14 +16,18 @@ int main (int argc, char **argv) {
 
 	struct bl_song song1;
 	struct bl_song song2;
+
 	float distance = bl_distance_file(filename1, filename2, &song1, &song2);
+	bl_free_song(&song1);
+	bl_free_song(&song2);
+
 	float similarity = bl_cosine_similarity_file(filename1, filename2, &song1, &song2);
+	bl_free_song(&song1);
+	bl_free_song(&song2);
 
     printf("Distance between %s and %s is: %f\n", filename1, filename2, distance);
     printf("Similarity between %s and %s is: %f\n", filename1, filename2, similarity);
 
-	bl_free_song(&song1);
-	bl_free_song(&song2);
 	return EXIT_SUCCESS;
 }
 
