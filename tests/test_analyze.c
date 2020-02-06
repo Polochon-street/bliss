26d701eef150f64e3804c2aa95769b4aef1ef793
Polochon-street <polochonstreet@gmx.fr>
2020-02-06 18:25:05 +0100

    Add an s32 song for testing resampling

--- tests/test_analyze.c
+++ tests/test_analyze.c
@@ -61,6 +61,41 @@ void test_analyze(void) {
 }
 
 
+void test_analyze_s32_(void) {
+    struct bl_song song;
+    bl_analyze("../audio/song_s32.flac", &song);
+
+    assert_floateq(song.force, -25.244282);
+
+    assert_floateq(song.force_vector.tempo, -8.218182);
+    assert_floateq(song.force_vector.amplitude, -15.064405);
+    assert_floateq(song.force_vector.frequency, -10.179875);
+    assert_floateq(song.force_vector.attack, -15.566257);
+    assert_eq(song.channels, 2);
+
+    assert_eq(song.nSamples, 488106);
+
+    assert_eq(song.sample_rate, 22050);
+
+    assert_eq(song.bitrate, 840742);
+    assert_eq(song.nb_bytes_per_sample, 2);
+
+    assert_eq(song.duration, 11);
+
+    assert_streq(song.artist, "David TMX");
+
+    assert_streq(song.title, "Renaissance");
+
+    assert_streq(song.album, "Renaissance");
+
+    assert_streq(song.tracknumber, "02");
+
+    assert_streq(song.genre, "Pop");
+    bl_free_song(&song);
+}
+
+
 int main(void) {
     test_analyze();
+    test_analyze_s32_();
 }
