4a41d784df306d90430e07b44ca6f23d1d14ad9c
Polochon-street <polochonstreet@gmx.fr>
2020-03-15 14:27:12 +0100

    Add first aubio stuff

--- bliss-rs/src/lib.rs
+++ bliss-rs/src/lib.rs
@@ -1,4 +1,5 @@
 // temporarily pub
+pub mod analyze;
 pub mod decode;
 
 pub const CHANNELS: u16 = 1;
@@ -6,11 +7,18 @@ pub const SAMPLE_RATE: u32 = 22050;
 
 #[derive(Default)]
 pub struct Song {
-    pub sample_array: Vec<i16>,
+    pub sample_array: Vec<f32>,
+    pub sample_rate: u32,
     pub file_path: String,
     pub artist: String,
     pub title: String,
     pub album: String,
     pub track_number: String,
     pub genre: String,
+}
+
+#[derive(Default)]
+pub struct Analysis {
+    pub tempo: f32,
+    pub spectral_centroid: f32,
 }
