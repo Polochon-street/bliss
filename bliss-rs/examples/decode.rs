b97ef81fe317430a0a3715707f52bdf087143afe
Polochon-street <polochonstreet@gmx.fr>
2020-03-18 19:53:56 +0100

    Convert sample_array to i16, move stuff around

--- bliss-rs/examples/decode.rs
+++ bliss-rs/examples/decode.rs
@@ -1,4 +1,4 @@
-use bliss_rs::decode_song;
+use bliss_rs::decode::decode_song;
 use ripemd160::{Digest, Ripemd160};
 use std::env;
 
@@ -7,7 +7,8 @@ fn main() {
     let path = &args[1];
     let song = decode_song(&path).unwrap();
     let mut hasher = Ripemd160::new();
-
-    hasher.input(song.sample_array);
+    for sample in song.sample_array.iter() {
+        hasher.input(sample.to_le_bytes().to_vec());
+    }
     println!("{:02x?}", hasher.result().as_slice());
 }
