09a28f264d8b001374b5c8a9620b03c8b5519fa8
Polochon-street <polochonstreet@gmx.fr>
2020-04-07 22:12:46 +0200

    Add ZCR to rust analysis

--- bliss-rs/src/utils.rs
+++ bliss-rs/src/utils.rs
@@ -2,20 +2,29 @@ pub fn mean<T: Clone + Into<f32>>(input: &[T]) -> f32 {
     input.iter().map(|x| x.clone().into() as f32).sum::<f32>() / input.len() as f32
 }
 
-pub fn number_crossings(input: &[f32]) -> usize {
-    let j: usize;
-    let mut ncr: usize = 0;
+// Essentia algorithm
+// https://github.com/MTG/essentia/blob/master/src/algorithms/temporal/zerocrossingrate.cpp
+pub fn number_crossings(input: &[f32]) -> u32 {
+    let mut crossings = 0;
+    let mut val = input[0];
 
-    for j in 1..input.len() {
-        if input[j - 1] < 0. {
-            if input[j] >= 0. {
-                ncr += 1;
-            }
-        } else {
-            if input[j] < 0. {
-                ncr += 1;
-            }
+    if val.abs() < 0. {
+        val = 0.
+    };
+    let mut was_positive = val > 0.;
+    let mut is_positive: bool;
+
+    for sample in input {
+        val = *sample;
+        if val.abs() <= 0.0 {
+            val = 0.0
+        };
+        is_positive = val > 0.;
+        if was_positive != is_positive {
+            crossings += 1;
+            was_positive = is_positive;
         }
     }
-    ncr
+
+    crossings
 }
