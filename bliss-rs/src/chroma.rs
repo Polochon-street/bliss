2aefcac271860366d811abe0281a1a1c3a9555a0
Polochon-street <polochonstreet@gmx.fr>
2020-09-30 22:51:28 +0200

    Add STFT + various tests

--- bliss-rs/src/chroma.rs
+++ bliss-rs/src/chroma.rs
@@ -5,114 +5,79 @@ extern crate ndarray;
 extern crate ndarray_npy;
 extern crate ndarray_stats;
 
+use crate::analyze::stft;
 use crate::utils::{hz_to_octs, median};
-use aubio_rs::vec::CVec;
-use aubio_rs::PVoc;
-use ndarray::{arr1, arr2, s, stack, Array, Array1, Array2, Axis, RemoveAxis};
+use ndarray::{arr1, s, stack, Array, Array1, Array2, Axis, RemoveAxis};
 use ndarray_stats::QuantileExt;
 
 pub struct ChromaDesc {
     sample_rate: u32,
     n_chroma: u32,
-    phase_vocoder: PVoc,
-    // 12 * nb_fft chroma values
     values_chroma: Array2<f64>,
-    collected_spectra: Array2<f64>,
 }
 
 impl ChromaDesc {
-    pub const WINDOW_SIZE: usize = 2048;
-    pub const HOP_SIZE: usize = 512;
+    pub const WINDOW_SIZE: usize = 8192;
+    pub const HOP_SIZE: usize = 2205;
 
     pub fn new(sample_rate: u32, n_chroma: u32) -> ChromaDesc {
         ChromaDesc {
             sample_rate,
             n_chroma,
-            values_chroma: arr2(&[[]]),
-            phase_vocoder: PVoc::new(ChromaDesc::WINDOW_SIZE, ChromaDesc::HOP_SIZE).unwrap(),
-            collected_spectra: arr2(&[[]]),
+            values_chroma: Array2::zeros((n_chroma as usize, 0)),
         }
     }
 
-    pub fn do_(&mut self, chunk: &[f32]) {
-        let mut fftgrain: Vec<f32> = vec![0.0; ChromaDesc::WINDOW_SIZE + 2];
-        self.phase_vocoder
-            .do_(chunk, fftgrain.as_mut_slice())
-            .unwrap();
-        let cvec: CVec = fftgrain.as_slice().into();
-        let norm: Array1<f64> = cvec.norm().iter().map(|x| *x as f64).collect();
-        if self.collected_spectra.is_empty() {
-            self.collected_spectra = norm.insert_axis(Axis(1));
-        }
-        else {
-            self.collected_spectra = stack![Axis(1), self.collected_spectra, norm.insert_axis(Axis(1))];
-        }
-        // If we have collected more than a hundred spectra
-        if self.collected_spectra.len_of(Axis(1)) >= 100 {
-            let chroma = chroma_stft(
-                self.sample_rate,
-                &self.collected_spectra,
-                ChromaDesc::WINDOW_SIZE as u32,
-                self.n_chroma,
-            );
-            if self.values_chroma.is_empty() {
-                self.values_chroma = chroma;
-            }
-            else {
-                self.values_chroma = stack![Axis(1), self.values_chroma, chroma];
-            }
-
-            self.collected_spectra = arr2(&[[]]);
-        }
-    }
-
-    pub fn finish(&mut self) {
-        if self.collected_spectra.len() <= 2 {
-            return;
-        }
+    pub fn do_(&mut self, signal: &[f32]) {
+        let stft = stft(signal, 8192, 2205);
+        let tuning = estimate_tuning(
+            self.sample_rate as u32,
+            &stft,
+            ChromaDesc::WINDOW_SIZE,
+            0.01,
+            12,
+        );
         let chroma = chroma_stft(
             self.sample_rate,
-            &self.collected_spectra,
-            ChromaDesc::WINDOW_SIZE as u32,
+            &stft,
+            ChromaDesc::WINDOW_SIZE,
             self.n_chroma,
+            Some(tuning),
         );
-        if self.values_chroma.is_empty() {
-                self.values_chroma = chroma;
-        }
-        else {
-            self.values_chroma = stack![Axis(1), self.values_chroma, chroma];
-        }
-        self.collected_spectra = arr2(&[[]]);
+        self.values_chroma = stack![Axis(1), self.values_chroma, chroma];
     }
 
-    // Doesn't make any sense now! Only here for the test
+    // Doesn't make any sense now! Only here for testing
     pub fn get_value(&mut self) -> f64 {
-        self.values_chroma
-            .iter()
-            .sum()
+        self.values_chroma.sum()
     }
 }
 
 // All the functions below are more than heavily inspired from
 // librosa's code: https://github.com/librosa/librosa/blob/main/librosa/feature/spectral.py#L1165
 // chroma(22050, n_fft=5, n_chroma=12)
-fn chroma_filter(sample_rate: u32, n_fft: u32, n_chroma: u32, tuning: f64) -> Array2<f64> {
+fn chroma_filter(sample_rate: u32, n_fft: usize, n_chroma: u32, tuning: f64) -> Array2<f64> {
     let ctroct = 5.0;
     let octwidth = 2;
 
-    let frequencies = Array::linspace(0., sample_rate as f64, (n_fft + 1) as usize);
-    let frequencies = frequencies.slice_move(s![1..-1]);
+    let frequencies = Array::linspace(0., f64::from(sample_rate), (n_fft + 1) as usize);
+    let length = frequencies.len();
+    let frequencies = frequencies.slice_move(s![1..length - 1]);
 
-    let freq_bins = n_chroma as f64 * hz_to_octs(&frequencies, tuning, n_chroma);
+    let freq_bins = f64::from(n_chroma) * hz_to_octs(&frequencies, tuning, n_chroma);
     let freq_bins = stack![
         Axis(0),
-        arr1(&[freq_bins[0] - 1.5 * n_chroma as f64]),
+        arr1(&[freq_bins[0] - 1.5 * f64::from(n_chroma)]),
         freq_bins
     ];
 
     let binwidth_bins = stack![
         Axis(0),
-        (&freq_bins.slice(s![1..]) - &freq_bins.slice(s![..-1])).mapv(|x| if x <= 1. { 1. } else { x }),
+        (&freq_bins.slice(s![1..]) - &freq_bins.slice(s![..-1])).mapv(|x| if x <= 1. {
+            1.
+        } else {
+            x
+        }),
         arr1(&[1.])
     ];
 
@@ -121,15 +86,15 @@ fn chroma_filter(sample_rate: u32, n_fft: u32, n_chroma: u32, tuning: f64) -> Ar
         row.fill(idx as f64);
     }
     let d = -a + &freq_bins;
-    let n_chroma2 = (n_chroma as f64 / 2.0).round() as u32;
+    let n_chroma2 = (f64::from(n_chroma) / 2.0).round() as u32;
 
-    let d = (d + n_chroma2 as f64 + 10. * n_chroma as f64) % n_chroma as f64 - n_chroma2 as f64;
+    let d = (d + f64::from(n_chroma2) + 10. * f64::from(n_chroma)) % f64::from(n_chroma) - f64::from(n_chroma2) as f64;
     let mut a: Array2<f64> = Array::zeros((n_chroma as usize, binwidth_bins.len()));
     for mut row in a.genrows_mut() {
         row.assign(&binwidth_bins);
     }
     let mut wts = (-0.5 * (2. * d / a).mapv(|x| x.powf(2.))).mapv(f64::exp);
-    //// Normalize by computing the l2-norm over the columns
+    // Normalize by computing the l2-norm over the columns
     for mut col in wts.gencolumns_mut() {
         let mut sum = (&col * &col).sum().sqrt();
         if sum < f64::MIN_POSITIVE {
@@ -142,7 +107,7 @@ fn chroma_filter(sample_rate: u32, n_fft: u32, n_chroma: u32, tuning: f64) -> Ar
     for mut row in scaling.genrows_mut() {
         row.assign(
             &(-0.5
-                * ((&freq_bins / n_chroma as f64 - ctroct) / octwidth as f64).mapv(|x| x.powf(2.)))
+                * ((&freq_bins / f64::from(n_chroma) - ctroct) / f64::from(octwidth)).mapv(|x| x.powf(2.)))
             .mapv(f64::exp),
         );
     }
@@ -163,17 +128,18 @@ fn chroma_filter(sample_rate: u32, n_fft: u32, n_chroma: u32, tuning: f64) -> Ar
     wts.slice_move(s![.., ..non_aliased])
 }
 
-fn pip_track(sample_rate: u32, spectrum: &Array2<f64>, n_fft: u32) -> (Array2<f64>, Array2<f64>) {
+fn pip_track(sample_rate: u32, spectrum: &Array2<f64>, n_fft: usize) -> (Array2<f64>, Array2<f64>) {
     let fmin = 150.0_f64;
-    let fmax = 4000.0_f64.min(sample_rate as f64 / 2.0);
+    let fmax = 4000.0_f64.min(f64::from(sample_rate) / 2.0);
     let threshold = 0.1;
 
-    let fft_freqs = Array::linspace(0., sample_rate as f64 / 2., (1 + n_fft / 2) as usize);
+    let fft_freqs = Array::linspace(0., f64::from(sample_rate) / 2., 1 + n_fft / 2);
 
     let avg = 0.5 * (&spectrum.slice(s![2.., ..]) - &spectrum.slice(s![..-2, ..]));
-    let shift = 2. * &spectrum.slice(s![1..-1, ..])
-        - &spectrum.slice(s![2.., ..])
-        - &spectrum.slice(s![0..-2, ..]);
+    let length = spectrum.len_of(Axis(0));
+    let shift = 2. * &spectrum.slice(s![1..length - 1, ..])
+        - spectrum.slice(s![2.., ..])
+        - spectrum.slice(s![0..length - 2, ..]);
 
     // TODO find more optimal stuff
     let shift = &avg
@@ -223,7 +189,7 @@ fn pip_track(sample_rate: u32, spectrum: &Array2<f64>, n_fft: u32) -> (Array2<f6
     let mut mags = Array::zeros(spectrum.raw_dim());
 
     for (i, j) in idx {
-        pitches[[i, j]] = (i as f64 + shift[[i, j]]) * sample_rate as f64 / n_fft as f64;
+        pitches[[i, j]] = (i as f64 + shift[[i, j]]) * f64::from(sample_rate) / n_fft as f64;
         mags[[i, j]] = spectrum[[i, j]] + dskew[[i, j]];
     }
     (pitches, mags)
@@ -235,11 +201,11 @@ fn pitch_tuning(frequencies: &Array1<f64>, resolution: f64, bins_per_octave: u32
         .filter(|x| **x > 0.)
         .map(|x| *x as f64)
         .collect::<Array1<f64>>();
-    
+
     if frequencies.is_empty() {
         return 0.0;
     }
-    let frequencies = bins_per_octave as f64 * hz_to_octs(&frequencies, 0.0, 12) % 1.0;
+    let frequencies = f64::from(bins_per_octave) * hz_to_octs(&frequencies, 0.0, 12) % 1.0;
 
     let residual = frequencies.mapv(|x| if x >= 0.5 { x - 1. } else { x });
 
@@ -258,7 +224,7 @@ fn pitch_tuning(frequencies: &Array1<f64>, resolution: f64, bins_per_octave: u32
 fn estimate_tuning(
     sample_rate: u32,
     spectrum: &Array2<f64>,
-    n_fft: u32,
+    n_fft: usize,
     resolution: f64,
     bins_per_octave: u32,
 ) -> f64 {
@@ -270,15 +236,17 @@ fn estimate_tuning(
         .map(|((i, j), _)| (i, j))
         .collect::<Vec<(usize, usize)>>();
 
-    let mut threshold = 0.0;
-    
     // TODO change that to Array1 stuff when bulk-indexing will be supported
-    if !pitches_index.is_empty() {
-        let mags = pitches_index.iter()
-            .map(|(i, j)| mag[[*i, *j]])
-            .collect::<Vec<f64>>();
-        threshold = median(&mags);
-    }
+    let threshold = {
+        if !pitches_index.is_empty() {
+            let mags = pitches_index
+                .iter()
+                .map(|(i, j)| mag[[*i, *j]])
+                .collect::<Vec<f64>>();
+            median(&mags)
+        }
+        else { 0. }
+    };
 
     let pitch = pitches_index
         .iter()
@@ -292,20 +260,25 @@ fn estimate_tuning(
 pub fn chroma_stft(
     sample_rate: u32,
     spectrum: &Array2<f64>,
-    n_fft: u32,
+    n_fft: usize,
     n_chroma: u32,
+    tuning: Option<f64>,
 ) -> Array2<f64> {
-    let tuning = estimate_tuning(sample_rate, &spectrum, n_fft, 0.01, n_chroma);
+    let tuning = match tuning {
+        Some(x) => x,
+        None => estimate_tuning(sample_rate, &spectrum, n_fft, 0.01, n_chroma),
+    };
+    let spectrum = &spectrum.mapv(|x| x.powf(2.));
     let chromafb = chroma_filter(sample_rate, n_fft, n_chroma, tuning);
 
     let mut raw_chroma = chromafb.dot(spectrum);
     for mut row in raw_chroma.gencolumns_mut() {
-        let mut max = *row.mapv(f64::abs).max().unwrap();
-        if max < f64::MIN_POSITIVE {
-            max = 1.;
+        let mut sum = row.mapv(|x| x.powf(2.)).sum().sqrt();
+        if sum < f64::MIN_POSITIVE {
+            sum = 1.;
         }
-        let max_row = Array::from_elem(row.raw_dim(), max);
-        row.assign(&(row.to_owned() / max_row));
+        let sum_row = Array::from_elem(row.raw_dim(), sum);
+        row.assign(&(row.to_owned() / sum_row));
     }
     raw_chroma
 }
@@ -313,32 +286,33 @@ pub fn chroma_stft(
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::analyze::stft;
     use crate::decode::decode_song;
-    use ndarray::{Array2};
+    use ndarray::Array2;
     use ndarray_npy::ReadNpyExt;
     use std::fs::File;
 
     #[test]
     fn test_chroma_desc() {
-        let song = decode_song("data/piano.flac").unwrap();
+        let song = decode_song("data/s16_mono_22_5kHz.flac").unwrap();
         let mut chroma_desc = ChromaDesc::new(song.sample_rate, 12);
-        for chunk in song.sample_array.chunks_exact(ChromaDesc::HOP_SIZE) {
-            chroma_desc.do_(&chunk);
-        }
-        chroma_desc.finish();
-
-        assert!((481.0289 - chroma_desc.get_value()).abs() < 0.001);
+        chroma_desc.do_(&song.sample_array);
+        assert!((263.7979324- chroma_desc.get_value()).abs() < 0.00001);
     }
 
     #[test]
-    fn test_chroma_stft() {
-        let file = File::open("data/spectrum-chroma.npy").unwrap();
-        let spectrum = Array2::<f64>::read_npy(file).unwrap();
+    fn test_chroma_stft_decode() {
+        let signal = decode_song("data/s16_mono_22_5kHz.flac")
+            .unwrap()
+            .sample_array;
+        let stft = stft(&signal, 8192, 2205);
 
-        let chroma = chroma_stft(22050, &spectrum, 2048, 12);
+        let file = File::open("data/chroma.npy").unwrap();
+        let expected_chroma = Array2::<f64>::read_npy(file).unwrap();
 
-        let chroma_stft_file = File::open("data/chroma-stft-normalized-expected.npy").unwrap();
-        let expected_chroma = Array2::<f64>::read_npy(chroma_stft_file).unwrap();
+        let chroma = chroma_stft(22050, &stft, 8192, 12, Some(-0.04999999999999999));
+
+        assert!(!chroma.is_empty() && !expected_chroma.is_empty());
 
         for (expected, actual) in expected_chroma.iter().zip(chroma.iter()) {
             assert!(0.0000001 > (expected - actual).abs());
@@ -352,6 +326,17 @@ mod test {
 
         let tuning = estimate_tuning(22050, &arr, 2048, 0.01, 12);
         assert!(0.000001 > (-0.09999999999999998 - tuning).abs());
+    }
+
+    #[test]
+    fn test_estimate_tuning_decode() {
+        let signal = decode_song("data/s16_mono_22_5kHz.flac")
+            .unwrap()
+            .sample_array;
+        let stft = stft(&signal, 8192, 2205);
+
+        let tuning = estimate_tuning(22050, &stft, 8192, 0.01, 12);
+        assert!(0.000001 > (-0.04999999999999999 - tuning).abs());
     }
 
     #[test]
@@ -390,7 +375,7 @@ mod test {
     }
 
     #[test]
-    fn test_generate_chroma() {
+    fn test_chroma_filter() {
         let file = File::open("data/chroma-filter.npy").unwrap();
         let expected_filter = Array2::<f64>::read_npy(file).unwrap();
 
