53d8181b91a42a9ce0fafbb4334eba4d93497a67
Polochon-street <polochonstreet@gmx.fr>
2021-01-20 00:08:21 +0100

    Add some utilities for analysis

--- bliss-rs/examples/analyze.rs
+++ bliss-rs/examples/analyze.rs
@@ -2,18 +2,17 @@ use std::env;
 use bliss_rs::Song;
 
 /**
- * Simple utility to print the result or the field of an Analysis.
+ * Simple utility to print the result of an Analysis.
  *
- * Takes a list of files to analyze and outputs a field // the result of
- * the corresponding Analysis, depending of what you chose to `println!` below.
+ * Takes a list of files to analyze an the result of the corresponding Analysis.
  */
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     for path in &args {
         match Song::new(&path) {
             Ok(song) => println!(
-                "{}: {}",
-                path, song.analysis.tempo,
+                "{}: {:?}",
+                path, song.analysis,
             ),
             Err(e) => println!("{}: {}", path, e),
         }
