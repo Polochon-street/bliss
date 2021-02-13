b935a10c65a22950a207cbfd043b79f9981387ff
Polochon-street <polochonstreet@gmx.fr>
2021-02-13 20:09:47 +0100

    Add a Library trait

--- bliss-rs/src/lib.rs
+++ bliss-rs/src/lib.rs
@@ -7,122 +7,15 @@ pub mod song;
 pub mod temporal;
 pub mod timbral;
 pub mod utils;
-#[macro_use]
-extern crate lazy_static;
+pub mod library;
+
 extern crate crossbeam;
 extern crate num_cpus;
-use ndarray::{arr2, Array2};
 
 pub const CHANNELS: u16 = 1;
 pub const SAMPLE_RATE: u32 = 22050;
 
-lazy_static! {
-    /// Covariance matrix for Mahalanobis distance.
-    /// See https://lelele.io/thesis.pdf, section 4.
-    static ref M: Array2<f32> = arr2(&[
-        [
-            0.0252749,
-            -0.01687417,
-            -0.0127546,
-            -0.00482922,
-            -0.02494876,
-            -0.00214683,
-            0.06241617,
-            -0.03128464,
-            -0.0058537
-        ],
-        [
-            -0.01687417,
-            0.05556368,
-            0.08265444,
-            -0.0265552,
-            -0.09629444,
-            0.00853458,
-            -0.03379969,
-            0.02132666,
-            0.00052228
-        ],
-        [
-            -0.0127546,
-            0.08265444,
-            0.1666087,
-            -0.04609117,
-            -0.12301082,
-            0.07466383,
-            0.01525878,
-            -0.0128122,
-            -0.00777542
-        ],
-        [
-            -0.00482922,
-            -0.0265552,
-            -0.04609117,
-            0.02578383,
-            0.09469197,
-            0.01697223,
-            -0.00968029,
-            0.00318867,
-            -0.00242896
-        ],
-        [
-            -0.02494876,
-            -0.09629444,
-            -0.12301082,
-            0.09469197,
-            0.42204981,
-            0.1233608,
-            -0.01607664,
-            -0.01726619,
-            -0.00726132
-        ],
-        [
-            -0.00214683,
-            0.00853458,
-            0.07466383,
-            0.01697223,
-            0.1233608,
-            0.18282812,
-            0.07852367,
-            -0.05310373,
-            -0.03115961
-        ],
-        [
-            0.06241617,
-            -0.03379969,
-            0.01525878,
-            -0.00968029,
-            -0.01607664,
-            0.07852367,
-            0.19510575,
-            -0.10635691,
-            -0.02719779
-        ],
-        [
-            -0.03128464,
-            0.02132666,
-            -0.0128122,
-            0.00318867,
-            -0.01726619,
-            -0.05310373,
-            -0.10635691,
-            0.06299881,
-            0.01300533
-        ],
-        [
-            -0.0058537,
-            0.00052228,
-            -0.00777542,
-            -0.00242896,
-            -0.00726132,
-            -0.03115961,
-            -0.02719779,
-            0.01300533,
-            0.00895838
-        ]
-    ]);
-}
-
-#[derive(Default, Debug, PartialEq)]
+#[derive(Default, Debug, PartialEq, Clone)]
 pub struct Song {
     // TODO make private
     pub sample_array: Option<Vec<f32>>,
@@ -135,17 +28,27 @@ pub struct Song {
     pub genre: String,
     /// Vec containing analysis, in order: tempo, spectral_centroid,
     /// zero_crossing_rate, spectral_rolloff, spectral_flatness, loudness,
-    /// is_major, fifth_0, fifth_1
+    /// chroma interval feature 1 to 10 
     pub analysis: Vec<f32>,
 }
 
+
+/// Simple function to bulk analyze a set of songs represented by their
+/// absolute paths.
+///
+/// When making an extension for an audio player, prefer
+/// implementing the `Library` trait.
 pub fn bulk_analyse(paths: Vec<String>) -> Vec<Result<Song, String>> {
     let mut songs = Vec::with_capacity(paths.len());
     let num_cpus = num_cpus::get();
 
     crossbeam::scope(|s| {
         let mut handles = Vec::with_capacity(paths.len() / num_cpus);
-        for chunk in paths.chunks(paths.len() / num_cpus) {
+        let mut chunk_number = paths.len() / num_cpus;
+        if chunk_number == 0 {
+            chunk_number = paths.len();
+        }
+        for chunk in paths.chunks(chunk_number) {
             handles.push(s.spawn(move |_| {
                 let mut result = Vec::with_capacity(chunk.len());
                 for path in chunk {
